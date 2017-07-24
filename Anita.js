bff3d1cdd50ac69f1ca6f55abf6b579508886c67
ivybina <ivybina999@gmail.com>
2017-07-24 23:11:15 +0300

    icon

--- Anita.js
+++ Anita.js
@@ -24,3 +24,24 @@ $(document).ready(function() {
         // find any other button with a specific id, and change it back to white like
         $('button#red').css('border', '1px solid white');
       }
+      /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+Try it Yourself »
