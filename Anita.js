4f05e4e5f6a598561b593d146452b7ca192f5dd8
ivybina <ivybina999@gmail.com>
2017-07-24 21:04:33 +0300

    interactive buttons

--- Anita.js
+++ Anita.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
-        $(".clickable").click(function() {
+        $(".show-gallery").click(function() {
           $(".show-gallery").toggle();
           $(".hide-gallery").toggle();
         });
       });
+      $('.Fastfood').click(function () {
+        $(this).css('border', '1px solid red');
+        // find any other button with a specific id, and change it back to white like
+        $('button#red').css('border', '1px solid white');
+      }
+      $('.Drinks').click(function () {
+        $(this).css('border', '1px solid red');
+        // find any other button with a specific id, and change it back to white like
+        $('button#red').css('border', '1px solid white');
+      }
+      $('.Chicken').click(function () {
+        $(this).css('border', '1px solid red');
+        // find any other button with a specific id, and change it back to white like
+        $('button#red').css('border', '1px solid white');
+      }
+      $('.Rice').click(function () {
+        $(this).css('border', '1px solid red');
+        // find any other button with a specific id, and change it back to white like
+        $('button#red').css('border', '1px solid white');
+      }
